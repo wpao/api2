5a1cfe8facc6bca6a6991a0301037b06e0ac809b
wpao <wpaozan@gmail.com>
2023-03-15 14:31:46 +0800

    tes v0.0.5

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 // //
-// const blogRoutes = require('./src/routes/blog');
+const blogRoutes = require('./src/routes/blog');
 // const multer = require('multer')
 // const path = require('path')
 
