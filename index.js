e660f976427d8cae31cdb44b0af442b0f06fcaab
wpao <wpaozan@gmail.com>
2023-03-15 14:52:50 +0800

    tes v0.0.8

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+const path = require('path')
+
+//accest img
+app.get('/images', express.static(path.join(__dirname, 'images')))
 
 app.get("/", (req, res) => res.send("Hello world"));
 app.get("/ping", (req, res) => res.send("Pong"));
